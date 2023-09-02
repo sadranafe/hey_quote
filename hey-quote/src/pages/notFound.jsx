1af0266f9037d1b694ab257750b1cf3ab080ea1b
mohammad sadra nafe <sadranafe7@gmail.com>
2023-09-03 00:04:59 +0330

    the structrue of the 404 Page added to notFound.jsx

--- hey-quote/src/pages/notFound.jsx
+++ hey-quote/src/pages/notFound.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
-
 const NotFound = () => {
-    return (
-        <>
-               
-        </>
-    );
+  return (
+    <>
+      <div className = "flex flex-wrap justify-center items-center">
+        <img className = "w-[800px]" src = "https://img.freepik.com/free-vector/404-error-with-landscape-concept-illustration_114360-7898.jpg" alt = "mohammad sadra nafe" />
+      </div>
+    </>
+  );
 };
 
 export default NotFound;
