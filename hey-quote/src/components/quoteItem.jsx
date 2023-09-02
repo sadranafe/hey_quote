c3ad72b5280c58356540252b92ff7a999ab4652b
mohammad sadra nafe <sadranafe7@gmail.com>
2023-09-02 21:34:11 +0330

    the structure of the quoteItem added in quoteItem.jsx

--- hey-quote/src/components/quoteItem.jsx
+++ hey-quote/src/components/quoteItem.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import { Link } from "react-router-dom"
 
-const QuoteItem = () => {
+const QuoteItem = ({text , author , id}) => {
     return (
         <>
-            <>
-                
-            </>   
+            <div className = "bg-4 m-2 p-3 rounded-lg w-6/12 flex flex-wrap justify-between items-center">
+                <div className = "w-8/12">
+                    <div>
+                        <h1 className = "text-xl">{text}</h1>
+                    </div>
+
+                    <div>
+                        <p>{author}</p>
+                    </div>
+                </div>
+
+                <Link to = {`/quotes/${id}`} className = "capitalize rounded-lg p-2 border border-stone-400 hover:bg-slate-700 hover:border-slate-700 hover:text-white transition-all">view fullScreen</Link>
+            </div>
         </>
     );
 };
