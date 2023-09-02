f32a8a295b3a3690790904bbcc874f95590b8ab8
mohammad sadra nafe <sadranafe7@gmail.com>
2023-09-02 20:12:30 +0330

    quoteItem.jsx added to working directory for rendering each data in AllQuotes.jsx

---
+++ hey-quote/src/components/quoteItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const QuoteItem = () => {
+    return (
+        <>
+            <>
+                
+            </>   
+        </>
+    );
+};
+
+export default QuoteItem;
