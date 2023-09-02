765fea6725b6fa6b92e0669429e3974c91b5ddce
mohammad sadra nafe <sadranafe7@gmail.com>
2023-09-02 21:35:30 +0330

    quoteItem.jsx imported in allQuote.jsx and map over the DUMMY_DATA and pass the data to quoteItem.jsx

--- hey-quote/src/pages/allQuote.jsx
+++ hey-quote/src/pages/allQuote.jsx
@@ -1,9 +1,15 @@
-import React from 'react';
+import QuoteItem from "../components/quoteItem";
 
-const AllQuote = () => {
+const AllQuote = ({dummy_data}) => {
     return (
         <>
-            
+            <div className = "flex flex-wrap mt-10 justify-center items-center content-center">
+                {
+                    dummy_data.map( (quote , index) =>
+                        <QuoteItem key = {index} id = {index} author = {quote.author} text = {quote.text}/>   
+                    )
+                }
+            </div>
         </>
     );
 };
